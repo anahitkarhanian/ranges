f91c52077a66960375556721e1e5634551d0af06
anahitkarhanian <anahit.karhanyan777@gmail.com>
2021-04-30 11:24:48 +0400

    Fixed ranges from 45 to 100

--- src/ranges.java
+++ src/ranges.java
@@ -4,18 +4,18 @@ public class ranges {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        number = (number + 45) / 45;
+        number = (number + 100) / 100;
         switch (number){
             case 1: {
-                System.out.println("The number is in range of [0,45)");
+                System.out.println("The number is in range of [0,100)");
                 break;
             }
             case 2: {
-                System.out.println("The number is in range of [45,90)");
+                System.out.println("The number is in range of [100,199)");
                 break;
             }
             case 3: {
-                System.out.println("The number is in range of [90,135)");
+                System.out.println("The number is in range of [200,299)");
                 break;
             }
             default: {
